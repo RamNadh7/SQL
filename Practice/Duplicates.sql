15b40bab3c30a26457791f2f9dbc4500509c8c5a
Ramanadham Pokuri <ramanadh99@gmail.com>
2025-06-13 20:14:54 +0530

    Add SQL script to delete duplicate records from cars table

---
+++ Practice/Duplicates.sql
@@ -0,0 +1,47 @@
+/* ##########################################################################
+   <<<<>>>> Scenario 1: Data duplicated based on SOME of the columns <<<<>>>>
+   ########################################################################## */
+
+-- Requirement: Delete duplicate data from cars table. Duplicate record is identified based on the model and brand name.
+
+drop table if exists cars;
+create table if not exists cars
+(
+    id      int,
+    model   varchar(50),
+    brand   varchar(40),
+    color   varchar(30),
+    make    int
+);
+insert into cars values (1, 'Model S', 'Tesla', 'Blue', 2018);
+insert into cars values (2, 'EQS', 'Mercedes-Benz', 'Black', 2022);
+insert into cars values (3, 'iX', 'BMW', 'Red', 2022);
+insert into cars values (4, 'Ioniq 5', 'Hyundai', 'White', 2021);
+insert into cars values (5, 'Model S', 'Tesla', 'Silver', 2018);
+insert into cars values (6, 'Ioniq 5', 'Hyundai', 'Green', 2021);
+
+select * from cars
+order by model, brand;
+
+---- using unique identifier
+
+delete from cars 
+where id in (
+select max(id)
+from cars
+group by model, brand
+having count(*)>1)
+
+
+---- Using SELF JOIN
+delete from cars where id in
+(
+select c2.id
+from cars c1
+join cars c2 on
+c1.model=c2.model and c1.brand=c2.brand
+where c1.id<c2.id)
+
+
+---- Window FUNCTION
+
